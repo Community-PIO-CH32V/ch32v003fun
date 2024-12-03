1602872476088dda56d42742869ed7e676b3370c
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2024-12-03 22:27:46 +0100

    Make Linux build static

--- minichlink/minichlink.c
+++ minichlink/minichlink.c
@@ -124,7 +124,7 @@ int main( int argc, char ** argv )
 		}
 	}
 
-#if !defined(WINDOWS) && !defined(WIN32) && !defined(_WIN32) && !defined(__APPLE__)
+#if !defined(WINDOWS) && !defined(WIN32) && !defined(_WIN32) && !defined(__APPLE__) && 0
 	{
 		uid_t uid = getuid();
 		struct passwd* pw = getpwuid(uid);
